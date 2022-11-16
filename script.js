841ebb62a1a4080ebd65da8a7a717503ef50d6aa
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-11-16 12:57:08 -0500

    Added userInput function and skeleton of for loops

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //link to grid space
 const gridSpace = document.getElementById("grid");
 
+//get user input
+let userInput = 16;
+function getUserInput() {
+    userInput = parseInt(prompt("How wide do you want the new grid to be?: "), 10);
+    if (userInput < 2 || userInput > 100) {
+        alert("ERROR: Please choose a number between 2 and 100, inclusive");
+        getUserInput();
+    }
+}
+
+//set up nested for-loops to create and append boxes by columns and rows
+for (let i = 0; i < userInput; i++) {
+    for(let j = 0; j < userInput; j++) {
+
+    }
+}
